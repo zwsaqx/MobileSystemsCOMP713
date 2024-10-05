bbc35357e47514264c16edd916450d1234f990ab
zwsaqx <77838223+zwsaqx@users.noreply.github.com>
2024-10-06 12:36:05 +1300

    buttons added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -59,6 +58,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.preference:preference:1.2.0")
     implementation("com.google.android.material:material:1.4.0")
+
+    //retrofit & gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
